95afd0ab6646273fa91f5f7c27803fe4a7c21721
cubanmjssle <cpulido1@student.gsu.edu>
2021-10-13 22:13:48 +0000

    initial commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import lyrics
 import waitress
 
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,7 +15,7 @@ def main(): ##hello world
 
     artist_data = rtn()
     lyricdata = lyrics.rtnURL()
-
+        
     return flask.render_template(
         "index.html",
         songName=artist_data['songName'],
@@ -23,16 +24,20 @@ def main(): ##hello world
         Audio=artist_data['Audio'],
         lyricsURL=lyricdata['lyricsURL']
     )
-
-#if __name__ == "__main__":
-  
 if __name__ == "__main__":
-        from waitress import serve
-        serve(app, host="0.0.0.0", port=8080)
-app.run()
+
+    app.run(host=os.getenv('IP', '0.0.0.0'),
+		port=int(os.getenv('PORT', 8080)),
+		debug=True
+    #from waitress import servehe
+    #serve
+    #(app, host="0.0.0.0", port=8080)
+)
 
     #app.run(host="localhost", port=8000, debug=True
  #host = '0.0.0.0',
         #port= int(os.getenv("PORT",8080)),
 #localhost
 #8000
+#####
+# Route for handling the login page logic
